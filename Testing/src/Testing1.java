0a40f85339c230e56635243bf0c7e04337aec3af
Jacksonchong11 <chong_qi_jun@soc.uum.edu.my>
2022-05-11 08:06:55 +0800

    8/5/2022

--- Testing/src/Testing1.java
+++ Testing/src/Testing1.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
-public class Testing1 {
-    public static void main(String[] args)
-   { 
-      int Test = 100; //Set the Test value here
-
-      System.out.printf("%-3d",12345);
-   }//end of main
-}//end of class
-
+public class Testing1
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner (System.in);
+        double midSem, assignment, quiz, carryMarks;
         
-
-
+        System.out.print("Enter midSem: ");
+        midSem = input.nextDouble();
+        System.out.print("Enter assignment mark: ");
+        assignment = input.nextDouble();
+        System.out.print("Enter quiz mark : ");
+        quiz = input.nextDouble(); 
+        carryMarks = midSem + assignment +  quiz;
         
-    
-
-   
-
+        System.out.println("Carry Marks is: " + carryMarks);
+        System.out.printf("Carry Marks in 1 decimal place: %.1f %n", carryMarks);
+    }
+}
